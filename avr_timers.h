f3b1772411e3207e4968e199253d76e4e631d318
Dmitry Reznikov <indraputra@yandex.ru>
2025-11-17 04:52:55 +0300

    timer2 in the works

--- avr_timers.h
+++ avr_timers.h
@@ -97,4 +97,58 @@ public:
 	//void pwmDisable(uint8_t ch_num);
 };
 
+
+class timer2
+{
+public:
+	uint8_t tccra, tccrb, ocra, timsk;
+	timer2 (uint8_t wgm, uint8_t prsclr = 1, uint8_t depth = 0);
+	void config(); // write set values into corresponding registers
+	//void enableOnly() {TCCR2B = tccrb;}
+	void enable(); // disable, config, start
+	void disable();
+	void stop() {TCCR2B &= ~0x7;}
+	void start() {TCCR2B = tccrb;}
+	uint8_t read() {return TCNT2;}
+	operator uint8_t() {return read();}
+	void write(uint8_t val) {TCNT2 = val;}
+	timer2& operator= (const uint8_t& val) {write(val); return *this;}
+	
+	void setMode(uint8_t wgm); //set the timer waveform mode
+	void setChannelA(uint8_t com, uint8_t pinmode = 1); //set COMxA value and output mode
+	void setChannelB(uint8_t com, uint8_t pinmode = 1); //set COMxB value and output mode
+	void setDepth(uint8_t res) {ocra = res;} //set depth/resolution/OCRA value
+	void setClock(uint8_t psc) {tccrb &= ~0x7; tccrb |= psc;} //set clock source/prescaler
+	void setPrescaler(uint8_t psc) {tccrb &= ~0x7; tccrb |= psc;} // compatibility/legacy
+	void setInterrupts(uint8_t t) {timsk&= ~0x7; timsk = t;}
+	
+	//void setPWMchannel(uint8_t ch_num, uint8_t mode, uint8_t plrty = 0); //plrty 0 is active high
+	//void pwmWrite(uint8_t ch_num, uint16_t val) {}
+	void writeA(uint8_t val) {OCR2A = val;} // write compare value A
+	void writeB(uint8_t val) {OCR2B = val;} // write compare value B
+	uint8_t readA() {return OCR2A;} // get the current compare value A
+	uint8_t readB() {return OCR2B;} // get the current compare value B
+	void channelA(uint8_t com, uint8_t pinmode = 1) 
+		{TCCR2A &= ~0xC0; TCCR2A |= com<<6; if (pinmode) DDRD |= 1<<6;} // change the channel output on the fly
+	void channelB(uint8_t com, uint8_t pinmode = 1)  
+		{TCCR2A &= ~0x30; TCCR2A |= com<<4; if (pinmode) DDRD |= 1<<5;}
+		
+	//void overflowInterrupt(uint8_t bit) {TIMSK2 &= ~0x01; TIMSK2 |= bit;}
+	//void compareAinterrupt(uint8_t bit) {TIMSK2 &= ~0x02; TIMSK2 |= bit<<1;}
+	//void compareBinterrupt(uint8_t bit) {TIMSK2 &= ~0x04; TIMSK2 |= bit<<2;}
+	
+	void OVFenable() {TIMSK2 |= 1;}
+	void COMPAenable() {TIMSK2 |= 2;}
+	void COMPBenable() {TIMSK2 |= 4;}
+	void OVFdisable() {TIMSK2 &= ~1;}
+	void COMPAdisable() {TIMSK2 &= ~2;}
+	void COMPBdisable() {TIMSK2 &= ~4;}
+	
+
+	
+	//void pwmEnable(uint8_t ch_num);
+	//void pwmDisable(uint8_t ch_num);
+};
+
+
 #endif
