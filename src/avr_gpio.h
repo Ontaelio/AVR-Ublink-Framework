547aa163ab81684a3608a2a6ea3da37c819d9fee
Dmitry Reznikov <indraputra@yandex.ru>
2026-03-22 14:27:09 +0300

    correct gpio

--- src/avr_gpio.h
+++ src/avr_gpio.h
@@ -33,9 +33,9 @@ public:
     inline void set()   { high(); }
     inline void reset() { low(); }
     
-    inline void write(uint8_t val) { *portx = (*portx & ~mask) | ((-val & 1) & mask); }
-    inline uint8_t read()     { return (*pinx & mask) != 0; }
-	inline void invert()            { *pinx = mask; } // toggle via PINx
+    inline void write(uint8_t val) { *portx = (*portx & ~mask) | ((!!val) * mask); }
+    inline uint8_t read() const { return (*pinx & mask) != 0; }
+	inline void invert() { *pinx = mask; } // toggle via PINx
 
 	void mode(uint8_t md);
 	
